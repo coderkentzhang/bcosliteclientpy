1d35a09799f5735d304c7c72bfcab6da2179d439
coderkentzhang <zkx@vip.qq.com>
2019-06-26 22:35:23 +0800

    remove markdown deps

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(
     install_requires=[
         'parsimonious>=0.8.0,<0.9.0',
     ],
-    setup_requires=['setuptools-markdown'],
+    setup_requires=[''], #setuptools-markdown
     python_requires='>=3.6, <4',
     extras_require=extras_require,
     py_modules=['bcosliteclientpy'],
